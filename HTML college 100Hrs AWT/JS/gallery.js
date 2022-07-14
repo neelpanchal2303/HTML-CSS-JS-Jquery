6bd1dc0c9f2a95761f4afe83fdc2aa8aa7a48e1f
Neel Panchal <108316104+neelpanchal2303@users.noreply.github.com>
2022-07-14 09:17:52 +0530

    Add files via upload

---
+++ HTML college 100Hrs AWT/JS/gallery.js
@@ -0,0 +1,10 @@
+
+    function myFunction(imgs) {
+        var expandImg = document.getElementById("expandedImg");
+        expandImg.src = imgs.src;
+      }
+  
+      function change(){
+          document.getElementById("text").style.color=prompt("Enter Color");
+          document.getElementById("text").style.fontFamily=prompt("Enter FontFaamily")
+      }
